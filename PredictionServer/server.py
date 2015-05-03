5278c3a2b221b7f4f66be2fa3002380b1b2f7f48
kjs <kjs@basalt>
2015-05-03 14:34:07 -0700

    new model

--- PredictionServer/server.py
+++ PredictionServer/server.py
@@ -42,7 +42,7 @@ def main():
     print >>sys.stderr, 'starting up on %s port %s' % server_address
     sock.bind(server_address)
     sock.listen(1)
-    f = open('./lda_clf')
+    f = open('./lda_clf2')
 
     while True:
         print >>sys.stderr, 'waiting for a connection'
